de724bae2cc33ed70c26a32837a3cbf15ca9df71
Wang, Steven (NZ - Auckland) <swang5@deloitte.co.nz>
2018-08-17 10:36:29 +1200

    Simple Bank

--- marketplace/migrations/2_deploy_contracts.js
+++ marketplace/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Adoption = artifacts.require("Adoption");
+var SimpleBank = artifacts.require("SimpleBank");
 
 module.exports = function(deployer) {
     deployer.deploy(Adoption);
+    deployer.deploy(SimpleBank);
 }
