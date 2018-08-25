1fdb6bb850dccfebce3bdf80ecf6cff110b068d1
Wang, Steven (NZ - Auckland) <swang5@deloitte.co.nz>
2018-08-26 02:46:55 +1200

    add more test cases

--- test/marketplace.test.js
+++ test/marketplace.test.js
@@ -9,7 +9,7 @@ contract('Marketplace', function(accounts){
     var id
     const price = web3.toWei(1, "ether")
 
-    it("should add an goods with the provided name, price and ipfspic", async() => {
+    it("should allow add an goods with the provided name, price and ipfspic", async() => {
         const marketplace = await Marketplace.deployed()
 
         var eventEmitted = false
@@ -37,6 +37,33 @@ contract('Marketplace', function(accounts){
         assert.equal(result[5], alice, 'the address adding the item should be listed as the seller')
         assert.equal(result[6], emptyAddress, 'the buyer address should be set to 0 when an item is added')
         assert.equal(eventEmitted, true, 'adding an item should emit a For Sale event')
+    })
+
+    // test for failure if user does not send enough funds
+    it("should not allow someone to purchase a goods with insufficient money", async() => {
+        const marketplace = await Marketplace.deployed()
+        const smartcontract = await marketplace.getAddress()
+
+        const amount = web3.toWei(0.5, "ether")
+
+        var scBalanceBefore = await web3.eth.getBalance(smartcontract).toNumber()
+        var bobBalanceBefore = await web3.eth.getBalance(bob).toNumber()
+        console.log('smart contract balance before:' + scBalanceBefore)
+        console.log('bob balance before:' + bobBalanceBefore)
+
+        try {
+            await marketplace.buyGoods(id, {from: bob, value: amount})
+        } catch(err) {
+            console.log(err);
+        }
+
+        var scBalanceAfter = await web3.eth.getBalance(smartcontract).toNumber()
+        var bobBalanceAfter = await web3.eth.getBalance(bob).toNumber()
+        console.log('smart contract balance after:' + scBalanceAfter)
+        console.log('bob balance after:' + bobBalanceAfter)
+
+        assert.equal(scBalanceAfter, scBalanceBefore, "smart contract's balance should remain same")
+        assert.isBelow(bobBalanceAfter, bobBalanceBefore, "bob's balance should decrease due to gas costs")
     })
 
     it("should allow someone to purchase a goods", async() => {
@@ -79,6 +106,32 @@ contract('Marketplace', function(accounts){
         assert.isBelow(bobBalanceAfter, bobBalanceBefore - parseInt(price, 10), "bob's balance should be reduced by more than the price of the item (including gas costs)")
     })
 
+    // test for calls that are made by not the seller
+    it("should not allow someone to mark the goods as shipped who is not seller", async() => {
+        const marketplace = await Marketplace.deployed()
+
+        var eventEmitted = false
+
+        var event = marketplace.LogShipped()
+        await event.watch((err, res) => {
+            id = res.args.id.toString(10)
+            eventEmitted = true
+        })
+
+        try {
+            await marketplace.shipGoods(id, {from: bob})
+        } catch(err) {
+            console.log(err);
+        }
+
+        const result = await marketplace.fetchGoods.call(id)
+
+        console.log('id:' + result[0] + ', name:' + result[1] + ', price:' + result[2] + ', ipfspic:' + result[3] + ', status:' + result[4] + ', seller:' + result[5] + ', buyer:' + result[6])
+
+        assert.equal(eventEmitted, false, 'should not emit a Shipped event')
+        assert.equal(result[4].toString(10), 1, 'the state of the item should be "Sold" remain the same')
+    })
+
     it("should allow the seller to mark the goods as shipped", async() => {
         const marketplace = await Marketplace.deployed()
 
@@ -98,6 +151,45 @@ contract('Marketplace', function(accounts){
 
         assert.equal(eventEmitted, true, 'Shipping an item should emit a Shipped event')
         assert.equal(result[4].toString(10), 2, 'the state of the item should be "Shipped", which should be declared third in the State Enum')
+    })
+
+    // test for calls that are made by not the seller
+    it("should not allow the someone to mark the goods as received who is not buyer", async() => {
+        const marketplace = await Marketplace.deployed()
+        const smartcontract = await marketplace.getAddress()
+
+        var eventEmitted = false
+
+        var event = marketplace.LogReceived()
+        await event.watch((err, res) => {
+            id = res.args.id.toString(10)
+            eventEmitted = true
+        })
+
+        var scBalanceBefore = await web3.eth.getBalance(smartcontract).toNumber()
+        var aliceBalanceBefore = await web3.eth.getBalance(alice).toNumber()
+        console.log('smart contract balance before:' + scBalanceBefore)
+        console.log('alice balance before:' + aliceBalanceBefore)
+
+        try {
+            await marketplace.receiveGoods(id, {from: alice})
+        } catch(err) {
+            console.log(err);
+        }
+
+        var scBalanceAfter = await web3.eth.getBalance(smartcontract).toNumber()
+        var aliceBalanceAfter = await web3.eth.getBalance(alice).toNumber()
+        console.log('smart contract balance after:' + scBalanceAfter)
+        console.log('alice balance after:' + aliceBalanceAfter)
+
+        const result = await marketplace.fetchGoods.call(id)
+
+        console.log('id:' + result[0] + ', name:' + result[1] + ', price:' + result[2] + ', ipfspic:' + result[3] + ', status:' + result[4] + ', seller:' + result[5] + ', buyer:' + result[6])
+
+        assert.equal(result[4].toString(10), 2, 'the state of the item should be "Shipped",remain the same')
+        assert.equal(eventEmitted, false, 'should not emit a Received event')
+        assert.equal(scBalanceAfter, scBalanceBefore, "smart contract's balance should remain the same")
+        assert.isBelow(aliceBalanceAfter, aliceBalanceBefore, "alice's balance should remain the same")
     })
 
     it("should allow the buyer to mark the goods as received", async() => {
@@ -133,23 +225,88 @@ contract('Marketplace', function(accounts){
         assert.equal(scBalanceAfter, scBalanceBefore - parseInt(price, 10), "owner's balance should be increased by the price of the item")
         assert.equal(aliceBalanceAfter, aliceBalanceBefore + parseInt(price, 10), "alice's balance should be increase by the price of the item (including gas costs)")
     })
-    // Test for failing conditions in this contracts
-    // test that every modifier is working
 
-    // add Goods
-   // buy Goods
-     // test for failure if user does not send enough funds
     // test for purchasing an item that is not for Sale
+    it("should not allow someone to purchase a goods which is not for sale", async() => {
+        const marketplace = await Marketplace.deployed()
+        const smartcontract = await marketplace.getAddress()
 
+        const amount = web3.toWei(2, "ether")
 
-    // shipItem
+        var scBalanceBefore = await web3.eth.getBalance(smartcontract).toNumber()
+        var bobBalanceBefore = await web3.eth.getBalance(bob).toNumber()
+        console.log('smart contract balance before:' + scBalanceBefore)
+        console.log('bob balance before:' + bobBalanceBefore)
 
-    // test for calls that are made by not the seller
+        try {
+            await marketplace.buyGoods(id, {from: bob, value: amount})
+        } catch(err) {
+            console.log(err);
+        }
+
+        var scBalanceAfter = await web3.eth.getBalance(smartcontract).toNumber()
+        var bobBalanceAfter = await web3.eth.getBalance(bob).toNumber()
+        console.log('smart contract balance after:' + scBalanceAfter)
+        console.log('bob balance after:' + bobBalanceAfter)
+
+        assert.equal(scBalanceAfter, scBalanceBefore, "smart contract's balance should remain same")
+        assert.isBelow(bobBalanceAfter, bobBalanceBefore, "bob's balance should decrease due to gas costs")
+    })
+
+    it("should allow add another an goods with the provided name, price and ipfspic", async() => {
+        const marketplace = await Marketplace.deployed()
+
+        var eventEmitted = false
+
+        var event = marketplace.LogForSale()
+        await event.watch((err, res) => {
+            id = res.args.id.toString(10)
+            eventEmitted = true
+        })
+
+        const name = "iPad"
+        const ipfspic = "0x424oj2gu5k2hb3442b3"
+
+        const goodsCount = await marketplace.addGoods(name, price, ipfspic, {from: alice})
+        console.log("goods count:" + goodsCount)
+        console.log("id:" + id)
+        const result = await marketplace.fetchGoods.call(id)
+
+        console.log('id:' + result[0] + ', name:' + result[1] + ', price:' + result[2] + ', ipfspic:' + result[3] + ', status:' + result[4] + ', seller:' + result[5] + ', buyer:' + result[6])
+
+        assert.equal(result[1], name, 'the name of the last added item does not match the expected value')
+        assert.equal(result[2].toString(10), price, 'the price of the last added item does not match the expected value')
+        assert.equal(result[3], ipfspic, 'the state of the item should be "For Sale", which should be declared first in the State Enum')
+        assert.equal(result[4].toString(10), 0, 'the state of the item should be "For Sale", which should be declared first in the State Enum')
+        assert.equal(result[5], alice, 'the address adding the item should be listed as the seller')
+        assert.equal(result[6], emptyAddress, 'the buyer address should be set to 0 when an item is added')
+        assert.equal(eventEmitted, true, 'adding an item should emit a For Sale event')
+    })
+
     // test for trying to ship an item that is not marked Sold
+    it("should not allow the seller to mark the goods as shipped as the goods is not Sold", async() => {
+        const marketplace = await Marketplace.deployed()
 
-    // receiveItem
+        var eventEmitted = false
 
-    // test calling the function from an address that is not the buyer
-    // test calling the function on an item not marked Shipped
+        var event = marketplace.LogShipped()
+        await event.watch((err, res) => {
+            id = res.args.id.toString(10)
+            eventEmitted = true
+        })
+
+        try {
+            await marketplace.shipGoods(id, {from: alice})
+        } catch(err) {
+            console.log(err);
+        }
+
+        const result = await marketplace.fetchGoods.call(id)
+
+        console.log('id:' + result[0] + ', name:' + result[1] + ', price:' + result[2] + ', ipfspic:' + result[3] + ', status:' + result[4] + ', seller:' + result[5] + ', buyer:' + result[6])
+
+        assert.equal(eventEmitted, false, 'should not emit a Shipped event')
+        assert.equal(result[4].toString(10), 0, 'the state of the item should be "ForSale", remain the same')
+    })
 
 })
