e5402577b9d6dd99913a3f0da42af5e9f5e643f8
Wang, Steven (NZ - Auckland) <swang5@deloitte.co.nz>
2018-08-26 23:28:13 +1200

    switch to Marketplace

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ ReactDOM.render((
     <Provider store={store}>
       <Router history={history}>
         <Route path="/" component={App}>
-          <IndexRoute component={Home} />
+          <IndexRoute component={Marketplace} />
           <Route path="marketplace" component={UserIsAuthenticated(Marketplace)} />
           <Route path="profile" component={UserIsAuthenticated(Profile)} />
         </Route>
